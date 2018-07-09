9c3f1eeea4fea76c1c4cd65efb19f74427da08cd
Jrod19477069 <41011397+Jrod19477069@users.noreply.github.com>
2018-07-09 08:28:46 -0600

    Add files via upload

---
+++ classMain.java
@@ -0,0 +1,16 @@
+public class Main{
+  public static void main(String[] args){
+
+Vehicle car = new Vehicle(10,"blue"4,10);
+
+
+    System.out.println(car.size);
+    car.speedUP();
+
+
+  }
+}
+
+
+
+  
